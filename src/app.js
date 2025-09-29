2830f9e1559578260a8c5e69674f9391ab4099d3
valvaradop <122133270+valvaradop@users.noreply.github.com>
2025-09-29 15:40:22 +0000

    First Commit

--- src/app.js
+++ src/app.js
@@ -5,7 +5,24 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+
+let who = ["The dog", "My grandma", "The mailman", "My bird"];
+let action = ["ate", "peed", "crushed", "broke"];
+let what = ["my homework", "my phone", "the car"];
+let when = ["before the class", "when I was sleeping", "while I was exercising", "during my lunch", "while I was praying"];
+  
+
+window.onload = function excusa() {
+
+    let quien = who [Math.floor(Math.random ()* who.length)];
+    let accion= action [Math.floor(Math.random ()* action.length)];
+    let que = what [Math.floor(Math.random ()* what.length)];
+    let cuando = when [Math.floor(Math.random ()* when.length)];
+
+    let excusa= `${quien} ${accion} ${que} ${cuando} `;
+    document.getElementById("excuse").innerHTML=`<strong>${excusa} </strong>`;
+
+
+  console.log (excusa);
+
 };
